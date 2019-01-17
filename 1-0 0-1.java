7ebb072663e778b4718a70bce9c6b6e9a931fc0d
asaitov777 <46367530+asaitov777@users.noreply.github.com>
2019-01-17 23:05:42 +0600

    1-Замена переменной

---
+++ 1-0 0-1.java
@@ -0,0 +1,26 @@
+//Замена переменной 1-0 и 0-1
+
+public class Main 
+{
+public static void main(String[] args) 
+{
+int[] mass = {1, 1, 0, 0, 1, 0, 1, 1, 0, 1};
+int maxMass = mass.length;
+for(int i = 0; i < maxMass; i++)
+{
+System.out.print (i + "-" + mass[i]+" ");
+}
+System.out.println("");
+for(int i = 0; i < maxMass; i++)
+if(mass[i] == 1)
+{
+mass[i] = 0;
+}else mass[i] = 1;
+for(int i = 0; i < maxMass; i++)
+{
+System.out.print (i + "-" + mass[i]+" ");
+}
+    
+}
+  
+}
